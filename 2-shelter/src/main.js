3be83d113538effdc862afb7c365c1b3747cc4d0
Daniil Aksonov <aksunov1@gmail.com>
2026-06-08 19:32:03 +0200

    🎨 style: add styles for header (Mon, Jun 08, 2026 07:32:03 PM)

--- 2-shelter/src/main.js
+++ 2-shelter/src/main.js
@@ -2,3 +2,7 @@ import '@/styles/reset.scss';
 import '@/styles/variables.scss';
 import '@/styles/utilities.scss';
 import '@/styles/globals.scss';
+
+import '@/components/layout/header.scss';
+import '@/components/logo.scss';
+import '@/components/menu.scss';
