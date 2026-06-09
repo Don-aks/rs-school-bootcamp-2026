ff4d04993b2508e73d44c93c104252d7de93321c
Daniil Aksonov <aksunov1@gmail.com>
2026-06-09 13:35:57 +0200

    🎨 style: stylize remaining sections (Tue, Jun 09, 2026 01:35:57 PM)

--- 2-shelter/src/pages/index.js
+++ 2-shelter/src/pages/index.js
@@ -1,1 +1,8 @@
 import '@/components/sections/hero.scss';
+import '@/components/sections/about.scss';
+import '@/components/sections/pets.scss';
+import '@/components/sections/cta.scss';
+import '@/components/sections/donation.scss';
+
+import '@/components/slider.scss';
+import '@/components/pet-card.scss';
