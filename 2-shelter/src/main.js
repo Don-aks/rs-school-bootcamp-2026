1f7fc74f5f32ba2ce88a1d159a9fa6360df1de2f
Daniil Aksonov <aksunov1@gmail.com>
2026-06-12 12:47:29 +0200

    🐛 fix(fonts): add system font fallback (Fri, Jun 12, 2026 12:47:29 PM)

--- 2-shelter/src/main.js
+++ 2-shelter/src/main.js
@@ -2,6 +2,7 @@ import Preloader from '@/modules/Preloader';
 import PetCardsRenderer from '@/modules/features/PetCardsRenderer';
 import getPetCards from '@/modules/api/getPetCards';
 
+import '@/styles/fonts.scss';
 import '@/styles/reset.scss';
 import '@/styles/variables.scss';
 import '@/styles/utilities.scss';
